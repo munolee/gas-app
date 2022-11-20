c3072843f0ed197988c04ccf8ac14aae5c2aee4b
Ian <yyh_lee@fitpet.co.kr>
2022-11-20 17:56:05 +0900

    ✨ remove label 스크립트 수정

--- src/remove-label.ts
+++ src/remove-label.ts
@@ -1,18 +1,32 @@
 import { GithubScriptInput } from './types/github-script.type';
 
-const prList = async (githubScript: GithubScriptInput) => {
-    const { repo, owner } = githubScript.context.repo;
-    const prList = await githubScript.github.rest.pulls.list({
-        owner,
-        repo,
-    });
-    console.log('--------------- 🖨 Get PR List ---------------');
-    console.log('🔔 PR List: ', prList);
+const removeAlphaLabel = (githubScript: GithubScriptInput) => {
+  const { repo, owner } = githubScript.context.repo;
+
+  githubScript.github.rest;
+};
+
+async function findOpenPr(githubScript: GithubScriptInput) {
+  const { repo, owner } = githubScript.context.repo;
+  const prList = await githubScript.github.rest.pulls.list({
+    owner,
+    repo,
+  });
+  console.log('--------------- 🖨 Get PR List ---------------');
+  console.log('🔔 PR List: ', prList);
+
+  const prNumber = prList.data.find((head) => {
+    console.log(head.labels);
+    // head.labels.find(() => 'alpha', 'staging');
+  })?.number;
+
+  return prNumber;
 }
 
 export const removeLabel = async (githubScript: GithubScriptInput) => {
-    console.log('-- Print Github Script Payload --');
-    console.log(githubScript.context.payload);
+  console.log('-- Print Github Script Payload --');
+  console.log(githubScript.context.payload);
 
-    await prList(githubScript)
-}
+  // Get pull request number list has alpha/staging label
+  const prNumber = await findOpenPr(githubScript);
+};
