976a76fa82b3b9115c366699b8b626c17bf7cde6
Ian <yyh_lee@fitpet.co.kr>
2023-01-11 14:47:36 +0900

    build - build file

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,2 +1,3 @@
-import { removeLabel } from "./remove-label";
-export { removeLabel };
+import { removeLabel } from './remove-label';
+import { createRelease } from './deploy-main';
+export { removeLabel, createRelease };
